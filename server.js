57ceba2c9f053a8faf9e7a8516f311699418d209
RamonPapes <ramonpapes@gmail.com>
2023-02-21 16:50:20 -0300

    fiz: refactory code

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const path = require('path');
-const mime = require('mime');
-
 const app = express();
 
 app.use(express.static('public/src/pages'));
@@ -11,33 +8,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/src/pages/index.html');
 });
 
-
-// app.get('/dia1', function (req, res) {
-//   res.sendFile(__dirname + '/public/src/pages/dia1/index.html');
-// });
-
 //Rota para as páginas de cada dia
-
-// for (let i = 1; i <= 6; i++) {
-//   app.get(`/dia${i}`, (req, res) => {
-//     res.sendFile(__dirname + `/public/src/pages/dia${i}/index.html`);
-//   });
-// }
-
-
 app.get('/dia/:value', (req, res) =>{
-  console.log(req);
-  const  value  = req.params.value;
+  const value = req.params.value;
   res.sendFile(__dirname + `/public/src/pages/dia${value}/index.html`);
 });
 
-// app.get('/dia/:value', (req, res) => {
-//   const value = req.params.value;
-//   const filePath = `${__dirname}/public/src/pages/dia${value}/index.html`;
-//   res.sendFile(filePath);
-// });
-
 // Inicie o servidor
-app.listen(3000, function () {
+app.listen(3000, ()=>{
   console.log('Servidor iniciado na porta 3000!');
-});
+})
